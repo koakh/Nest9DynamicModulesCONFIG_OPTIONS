bf0412d670b36267bc579872d9cfb77ce40cc6d1
Mário Monteiro <marioammonteiro@gmail.com>
2022-09-08 00:46:55 +0100

    commit with minimal code do show error

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [ConfigModule.register({ folder: './config' }), TestModule],
+  imports: [
+    ConfigModule.register({ folder: './config' }),
+    TestModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
+  exports: [ConfigModule]
 })
-export class AppModule {}
+export class AppModule { }
--- src/test/test.module.ts
+++ src/test/test.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from 'src/config/config.module';
-import { ConfigService } from 'src/config/config.service';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
 import { TestController } from './test.controller';
 import { TestService } from './test.service';
 
 @Module({
   imports: [ConfigModule],
   controllers: [TestController],
-  providers: [TestService, ConfigService]
+  providers: [TestService, ConfigService],
 })
 export class TestModule {  
 }
